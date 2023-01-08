024c7e85aec229043e0056d482ea805092c65e82
starcoex <69073390+starcoex@users.noreply.github.com>
2023-01-08 17:59:08 +0900

    React To-do Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/GlobalStyle';
+
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { DefalutTheme } from 'styled-components';
+
+export const lightTheme: DefalutTheme = {
+	bgColor: '#efefef',
+	textColor: '#222222',
+	accentColor: 'royalblue',
+	cardColor: 'white',
+};
+export const darkTheme: DefalutTheme = {
+	bgColor: '#222222',
+	textColor: '#efefef',
+	accentColor: 'royalblue',
+	cardColor: '#3b3b3b',
+};
