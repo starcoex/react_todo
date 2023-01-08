506c04ff5286475106475359e2ce3791ef7f2233
starcoex <69073390+starcoex@users.noreply.github.com>
2023-01-08 18:30:21 +0900

    React Form first Setup

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,14 +2,20 @@ import React, { useState } from 'react'
 
 function ToDoList() {
   const [value, setValue] = useState("")
+  const [toDoError, setToDoError] = useState("")
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const { currentTarget: { value }, } = event
+    setToDoError("")
     setValue(value)
 
   }
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     console.log(value)
+    if (value.length < 10) {
+      return setToDoError("To do should be longer")
+    }
+    console.log("submit")
 
   }
   return (
@@ -17,6 +23,7 @@ function ToDoList() {
       <form action="" onSubmit={onSubmit}>
         <input value={value} onChange={onChange} type="text" placeholder='Write To do....' />
         <button>Adds</button>
+        {toDoError !== "" ? toDoError : null}
       </form>
     </div>
   )
